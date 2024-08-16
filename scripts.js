7974b396f28b27afce92f9a2fc57d45146aca46f
grantlandon <grant.landon4@gmail.com>
2024-08-15 20:57:08 -0400

    carrosel

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Auto-slide functionality (optional)
+setInterval(() => {
+    nextSlide();
+}, 3000); // Change slide every 3 seconds
